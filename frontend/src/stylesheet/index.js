84514ea80541ed367d4379978825e2a5dac647f0
james <jamesliwood@gmail.com>
2024-05-05 02:27:10 -0400

    moved App.css to a stylesheet

---
+++ frontend/src/stylesheet/index.js
@@ -0,0 +1,10 @@
+export { default as App } from "./app.css";
+export { default as Auth } from "./channel-container/auth.css";
+export { default as ChannelListContainer } from "./channel-list-container/channelListContainer.css";
+export { default as ChannelSearch } from "./channel-list-container/channelSearch.css";
+export { default as TeamChannelList } from "./channel-list-container/teamChannelList.css";
+export { default as TeamChannelPreview } from "./channel-list-container/teamChannelPreview.css";
+export { default as ChannelContainer } from "./channel-container/channelContainer.css";
+export { default as ChannelInner } from "./channel-container/channelInner.css";
+export { default as CreateChannel } from "./channel-container/createChannel.css";
+export { default as EditChannel } from "./channel-container/editChannel.css";
