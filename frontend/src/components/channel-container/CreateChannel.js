5820861fa211b38da57d82eafc3a5360af33c840
james <jamesliwood@gmail.com>
2024-05-11 03:24:24 -0400

    Updated components

--- frontend/src/components/channel-container/CreateChannel.js
+++ frontend/src/components/channel-container/CreateChannel.js
@@ -1,5 +1,76 @@
-const CreateChannel = () => {
-  return <div>Create Channel</div>;
+import { useState } from "react";
+import { UserList } from "..";
+import { useChatContext } from "stream-chat-react";
+
+const ChannelNameInput = ({ setChannelName }) => {
+  const handleChange = (e) => {
+    e.preventDefault();
+    setChannelName(e.target.value);
+  };
+
+  return (
+    <div className="create-channel-input">
+      <div>Name</div>
+      <input type="text" placeholder="channel-name" onChange={handleChange} />
+      <div>Add Members</div>
+    </div>
+  );
+};
+
+const CreateChannel = ({ createType, setIsCreating }) => {
+  const { client, setActiveChannel } = useChatContext();
+  const [selectedUsers, setSelectedUsers] = useState([client.userID] || "");
+  const [channelName, setChannelName] = useState("");
+  const [error, setError] = useState({ state: false, message: "" });
+
+  const channelCreation = async (e) => {
+    e.preventDefault();
+
+    if (createType == "team" && !channelName) {
+      setError({ ...error, state: true, message: "Channel name is empty" });
+      return;
+    }
+
+    try {
+      const newChannel = await client.channel(createType, channelName, {
+        name: channelName,
+        members: selectedUsers,
+      });
+      await newChannel.watch();
+
+      setChannelName("");
+      setIsCreating(false);
+      setSelectedUsers([client.userID]);
+      setActiveChannel(newChannel);
+    } catch (err) {
+      setError({
+        ...error,
+        state: true,
+        message: "You need at least 2 members to create a channel.",
+      });
+    }
+  };
+
+  return (
+    <div className="create-channel-container">
+      <div className="create-channel-header">
+        <div>
+          {createType == "team" ? "Create a New Channel" : "Direct Messaging"}
+        </div>
+        <button onClick={() => setIsCreating(false)}>x</button>
+      </div>
+      {createType == "team" && (
+        <ChannelNameInput setChannelName={setChannelName} />
+      )}
+      {error.state && (
+        <div className="create-channel-error">{error.message}</div>
+      )}
+      <UserList setSelectedUsers={setSelectedUsers} />
+      <button className="create-channel-button" onClick={channelCreation}>
+        {createType == "team" ? "Create Channel" : "Create Message Group"}
+      </button>
+    </div>
+  );
 };
 
 export default CreateChannel;
---
+++ frontend/src/components/channel-container/UserList.js
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+import { Avatar, useChatContext } from "stream-chat-react";
+
+const ListContainer = ({ children }) => {
+  return (
+    <div className="user-list-container">
+      <div className="user-list-header">
+        <div>User</div>
+        <div>Invite</div>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const UserItem = ({ user, setSelectedUsers }) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelected = () => {
+    if (selected) {
+      setSelectedUsers((prevUsers) =>
+        prevUsers.filter((prevUser) => prevUser !== user.id)
+      );
+    } else {
+      setSelectedUsers((prevUser) => [...prevUser, user.id]);
+    }
+    setSelected((prevState) => !prevState);
+  };
+
+  return (
+    <div className="user-item-wrapper" onClick={handleSelected}>
+      <div className="user-item-user-info">
+        <Avatar image={user.image} name={user.fullName || user.id} size={32} />
+        <div className="user-item-name">{user.fullName || user.id}</div>
+      </div>
+      <div className="user-item-invite">
+        {selected ? <div>selected</div> : <div>not selected</div>}
+      </div>
+    </div>
+  );
+};
+
+const UserList = ({ setSelectedUsers }) => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [listEmpty, setListEmpty] = useState(false);
+  const { client } = useChatContext();
+  const [error, setError] = useState(false);
+  
+  useEffect(() => {
+    const getUsers = async () => {
+      if (loading) return;
+
+      setLoading(true);
+
+      try {
+        const response = await client.queryUsers(
+          { id: { $ne: client.userID } },
+          { id: 1 },
+          { limit: 8 }
+        );
+
+        if (response.users.length) {
+          setUsers(response.users);
+        } else {
+          setListEmpty(true);
+        }
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+
+    if (client) getUsers();
+  }, []);
+
+  if (error) {
+    return (
+      <ListContainer>
+        <div>Error loading, please refresh and try again.</div>;
+      </ListContainer>
+    );
+  }
+
+  if (listEmpty) {
+    return (
+      <ListContainer>
+        <div>No users found</div>
+      </ListContainer>
+    );
+  }
+
+  return (
+    <ListContainer>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="user-item-container">
+          {users?.map((user) => {
+            return (
+              <UserItem
+                user={user}
+                key={user.id}
+                setSelectedUsers={setSelectedUsers}
+              />
+            );
+          })}
+        </div>
+      )}
+    </ListContainer>
+  );
+};
+
+export default UserList;
